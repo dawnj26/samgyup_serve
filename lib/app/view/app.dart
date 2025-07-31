0ec037164c0997ca7fc480b41f88f03c4bd18b54
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-07-31 17:44:27 +0800

    initialize repositories

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,10 +1,46 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:samgyup_serve/app/bloc/app_bloc.dart';
 import 'package:samgyup_serve/app/router/router.dart';
 import 'package:samgyup_serve/l10n/l10n.dart';
 
-class App extends StatelessWidget {
-  App({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
 
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late final AuthenticationRepository _authenticationRepository;
+
+  @override
+  void initState() {
+    _authenticationRepository = AuthenticationRepository();
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider.value(value: _authenticationRepository),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) =>
+                AppBloc(authenticationRepository: _authenticationRepository),
+          ),
+        ],
+        child: AppView(),
+      ),
+    );
+  }
+}
   final _appRouter = AppRouter();
 
   @override
