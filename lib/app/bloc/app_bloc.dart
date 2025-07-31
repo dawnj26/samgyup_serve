ef6b0ebca601c3bba5c5b59cee7402bc24c0c418
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-07-31 17:43:20 +0800

    add _onStarted event

--- lib/app/bloc/app_bloc.dart
+++ lib/app/bloc/app_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -9,8 +10,25 @@ class AppBloc extends Bloc<AppEvent, AppState> {
   AppBloc({
     required AuthenticationRepository authenticationRepository,
   }) : _authenticationRepository = authenticationRepository,
+       super(const Initial()) {
+    on<_Started>(_onStarted);
+  }
 
   final AuthenticationRepository _authenticationRepository;
 
+  Future<void> _onStarted(_Started event, Emitter<AppState> emit) async {
+    return emit.onEach(
+      _authenticationRepository.user,
+      onData: (user) {
+        emit(Authenticated(user: user));
+      },
+      onError: (error, stackTrace) {
+        emit(
+          Unauthenticated(
+            errorMessage: error.toString(),
+          ),
+        );
+      },
+    );
   }
 }
