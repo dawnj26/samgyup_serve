394737cdde1e31ce3de79686016f26301cc0cebc
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-07-31 17:43:57 +0800

    add initial, authenticated, and unauth states

--- lib/app/bloc/app_state.dart
+++ lib/app/bloc/app_state.dart
@@ -2,5 +2,11 @@ part of 'app_bloc.dart';
 
 @freezed
 class AppState with _$AppState {
-  const factory AppState.initial() = _Initial;
+  const factory AppState.initial() = Initial;
+  const factory AppState.authenticated({
+    required User user,
+  }) = Authenticated;
+  const factory AppState.unauthenticated({
+    String? errorMessage,
+  }) = Unauthenticated;
 }
