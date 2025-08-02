f92d17f352eec81ac63c059ad77beb197bbc1118
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-02 17:12:33 +0800

    route: add admin shell route

--- lib/app/router/router.dart
+++ lib/app/router/router.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:samgyup_serve/admin/admin.dart';
 import 'package:samgyup_serve/app/app.dart';
 import 'package:samgyup_serve/home/home.dart';
-import 'package:samgyup_serve/home/shell/home_shell_page.dart';
 import 'package:samgyup_serve/login/login.dart';
 
 part 'router.gr.dart';
@@ -10,7 +9,7 @@ part 'router.gr.dart';
 @AutoRouterConfig(replaceInRouteName: 'Screen|Page,Route')
 class AppRouter extends RootStackRouter {
   @override
-  RouteType get defaultRouteType => const RouteType.material(); //.cupertino, .adaptive ..etc
+  RouteType get defaultRouteType => const RouteType.material();
 
   @override
   List<AutoRoute> get routes => [
@@ -21,12 +20,15 @@ class AppRouter extends RootStackRouter {
         AutoRoute(
           page: HomeShellRoute.page,
           children: [
-            AutoRoute(page: LoginRoute.page),
             AutoRoute(page: HomeRoute.page, initial: true),
+            AutoRoute(page: LoginRoute.page),
           ],
         ),
         AutoRoute(
-          page: AdminRoute.page,
+          page: AdminShellRoute.page,
+          children: [
+            AutoRoute(page: AdminRoute.page, initial: true),
+          ],
         ),
       ],
     ),
