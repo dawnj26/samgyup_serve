d9a666766eedf0ec7484b3870d7310cbdf8a53e3
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-13 20:53:14 +0800

    add management items and model

---
+++ lib/data/management.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:samgyup_serve/data/models/management_item.dart';
+
+final List<ManagementItem> managementItems = [
+  const ManagementItem(
+    title: 'Inventory',
+    icon: Icons.inventory_2_rounded,
+  ),
+  const ManagementItem(
+    title: 'Orders',
+    icon: Icons.receipt_long_rounded,
+  ),
+  const ManagementItem(
+    title: 'Customers',
+    icon: Icons.people_rounded,
+  ),
+  const ManagementItem(
+    title: 'Settings',
+    icon: Icons.settings_rounded,
+  ),
+];
