0e5a05e8b8ed8916a2442d5109402a5bdc52c672
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-13 17:22:43 +0800

    add admin screen

---
+++ lib/admin/view/admin_screen.dart
@@ -0,0 +1,55 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:samgyup_serve/app/bloc/app_bloc.dart';
+import 'package:samgyup_serve/app/router/router.dart';
+import 'package:samgyup_serve/components/components.dart';
+
+class AdminScreen extends StatelessWidget {
+  const AdminScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final appBloc = context.read<AppBloc>();
+
+    return AutoTabsScaffold(
+      appBarBuilder: (context, tabsRouter) {
+        return AppBar(
+          title: const AppLogoIcon(
+            size: 40,
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.logout),
+              onPressed: () {
+                appBloc.add(const AppEvent.logout());
+              },
+            ),
+          ],
+        );
+      },
+      routes: const [
+        DashboardRoute(),
+        ManagementRoute(),
+      ],
+      bottomNavigationBuilder: (_, tabsRouter) {
+        return NavigationBar(
+          selectedIndex: tabsRouter.activeIndex,
+          onDestinationSelected: tabsRouter.setActiveIndex,
+          destinations: const [
+            NavigationDestination(
+              label: 'Home',
+              icon: Icon(Icons.dashboard_outlined),
+              selectedIcon: Icon(Icons.dashboard),
+            ),
+            NavigationDestination(
+              label: 'Management',
+              icon: Icon(Icons.settings_outlined),
+              selectedIcon: Icon(Icons.settings),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
