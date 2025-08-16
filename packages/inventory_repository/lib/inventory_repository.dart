79036dc43f66e53903915db985a271145c1d140c
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-16 13:19:41 +0800

    add inventory repository

---
+++ packages/inventory_repository/lib/inventory_repository.dart
@@ -0,0 +1,5 @@
+/// Repository for managing inventory data.
+library;
+
+export 'src/enums/enums.dart';
+export 'src/inventory_repository.dart';
---
+++ packages/inventory_repository/lib/src/inventory_repository.dart
@@ -0,0 +1,7 @@
+/// {@template inventory_repository}
+/// A repository for managing inventory data.
+/// {@endtemplate}
+class InventoryRepository {
+  /// {@macro inventory_repository}
+  const InventoryRepository();
+}
