45ae40d354308048eee43f8b0b7c78769e84ff76
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-16 18:10:44 +0800

    add create screen

---
+++ lib/ui/inventory/view/inventory_add_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InventoryAddScreen extends StatelessWidget {
+  const InventoryAddScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
