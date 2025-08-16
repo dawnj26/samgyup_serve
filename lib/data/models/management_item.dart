6aee64c2024a23966eed0c55b5eaf4c1ee1c1cee
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-16 12:44:29 +0800

    add optional route

--- lib/data/models/management_item.dart
+++ lib/data/models/management_item.dart
@@ -1,11 +1,14 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 class ManagementItem {
   const ManagementItem({
     required this.title,
     required this.icon,
+    this.route,
   });
 
   final String title;
   final IconData icon;
+  final PageRouteInfo? route;
 }
