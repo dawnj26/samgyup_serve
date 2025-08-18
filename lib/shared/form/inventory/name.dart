a92944fdb0dc61b9704acdbf98bb2ba8eac71393
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-18 16:19:33 +0800

    add name

---
+++ lib/shared/form/inventory/name.dart
@@ -0,0 +1,28 @@
+import 'package:formz/formz.dart';
+
+enum NameValidationError {
+  empty,
+  tooShort,
+}
+
+class Name extends FormzInput<String, NameValidationError> {
+  const Name.pure() : super.pure('');
+  const Name.dirty([super.value = '']) : super.dirty();
+
+  static const int _minLength = 3;
+
+  @override
+  NameValidationError? validator(String? value) {
+    final name = value ?? '';
+
+    if (name.isEmpty) {
+      return NameValidationError.empty;
+    }
+
+    if (name.length < _minLength) {
+      return NameValidationError.tooShort;
+    }
+
+    return null;
+  }
+}
