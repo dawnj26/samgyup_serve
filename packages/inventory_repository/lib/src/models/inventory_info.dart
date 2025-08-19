5618692b0e34e4a0f21fb4321c3ab98af0b62be1
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-19 21:40:19 +0800

    add inventory info model

---
+++ packages/inventory_repository/lib/src/models/inventory_info.dart
@@ -0,0 +1,34 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'inventory_info.freezed.dart';
+part 'inventory_info.g.dart';
+
+/// {@template inventory_info}
+/// A model representing inventory information.
+///
+/// Contains details about the total number of items and their statuses.
+/// {@endtemplate}
+@freezed
+abstract class InventoryInfo with _$InventoryInfo {
+  /// {@macro inventory_info}
+  const factory InventoryInfo({
+    required int totalItems,
+    required int inStockItems,
+    required int lowStockItems,
+    required int outOfStockItems,
+    required int expiredItems,
+  }) = _InventoryInfo;
+
+  /// Creates an empty [InventoryInfo] instance with default values.
+  factory InventoryInfo.empty() => const InventoryInfo(
+    totalItems: -1,
+    inStockItems: -1,
+    lowStockItems: -1,
+    outOfStockItems: -1,
+    expiredItems: -1,
+  );
+
+  /// Creates an instance of [InventoryInfo] from a JSON map.
+  factory InventoryInfo.fromJson(Map<String, dynamic> json) =>
+      _$InventoryInfoFromJson(json);
+}
