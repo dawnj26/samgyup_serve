667962cf54eb2fc42ee96d9e66b1a6af4a5bd98d
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-19 21:39:57 +0800

    revert add back document to json

--- packages/appwrite_repository/lib/src/appwrite_repository.dart
+++ packages/appwrite_repository/lib/src/appwrite_repository.dart
@@ -1,4 +1,5 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as appwrite_models;
 import 'package:appwrite_repository/src/models/models.dart';
 import 'package:intl/intl.dart';
 
@@ -98,5 +99,13 @@ class AppwriteRepository {
   /// @return [String] A formatted date.
   String _getCurrentDate() {
     return DateFormat('MMM dd, HH:mm').format(DateTime.now());
+  }
+
+  /// Converts an Appwrite Document to a JSON map.
+  Map<String, dynamic> documentToJson(appwrite_models.Document document) {
+    return {
+      ...document.data,
+      'updatedAt': document.$updatedAt,
+    };
   }
 }
