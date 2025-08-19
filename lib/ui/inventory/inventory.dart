1aed6d85b5681176abac3750134fee9b2e7f1486
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-19 21:39:31 +0800

    export

--- lib/ui/inventory/inventory.dart
+++ lib/ui/inventory/inventory.dart
@@ -1,3 +1,4 @@
 export 'shell/inventory_shell_page.dart';
 export 'view/create/inventory_add_page.dart';
 export 'view/inventory_page.dart';
+export 'view/list/status/inventory_status_list_page.dart';
