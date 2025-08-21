0b1791439abb8f205c70b55dcfba0dd00e099fce
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-21 19:04:20 +0800

    add app bar for category list

---
+++ lib/ui/inventory/components/category_list_app_bar.dart
@@ -0,0 +1,38 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:samgyup_serve/bloc/inventory/category/inventory_category_bloc.dart';
+import 'package:samgyup_serve/bloc/inventory/delete/inventory_delete_bloc.dart';
+
+class CategoryListAppBar extends StatelessWidget {
+  const CategoryListAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<InventoryCategoryBloc, InventoryCategoryState>(
+      buildWhen: (previous, current) => previous.category != current.category,
+      builder: (context, state) {
+        final colorScheme = Theme.of(context).colorScheme;
+        final category = state.category;
+
+        return SliverAppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              final triggerReload =
+                  context.read<InventoryDeleteBloc>().state
+                      is InventoryDeleteSuccess;
+              context.router.pop(triggerReload);
+            },
+          ),
+          pinned: true,
+          expandedHeight: 200,
+          backgroundColor: colorScheme.primaryContainer,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(category.label),
+          ),
+        );
+      },
+    );
+  }
+}
