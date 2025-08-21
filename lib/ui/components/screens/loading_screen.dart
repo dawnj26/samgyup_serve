e97c50ecca42f6cab1e598155ff748c6cc180198
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-21 19:02:59 +0800

    use app logo bounce for loading states

--- lib/ui/components/screens/loading_screen.dart
+++ lib/ui/components/screens/loading_screen.dart
@@ -1,14 +1,16 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:samgyup_serve/ui/components/app_logo_bounce.dart';
 
 @RoutePage()
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({required this.message, super.key});
-
   final String message;
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return PopScope(
       canPop: false,
       child: Scaffold(
@@ -17,10 +19,11 @@ class LoadingScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const CircularProgressIndicator(),
-                const SizedBox(height: 16),
+                const AppLogoBounce(),
+                const SizedBox(height: 24),
                 Text(
                   message,
+                  style: textTheme.titleMedium,
                 ),
               ],
             ),
