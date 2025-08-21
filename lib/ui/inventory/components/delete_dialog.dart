16bfb0b42e68f266e656e52904f91e0b5a378ef9
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-21 19:04:24 +0800

    add delete dialog

---
+++ lib/ui/inventory/components/delete_dialog.dart
@@ -0,0 +1,33 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:inventory_repository/inventory_repository.dart';
+
+class DeleteDialog extends StatelessWidget {
+  const DeleteDialog({required this.item, super.key, this.onDelete});
+
+  final InventoryItem item;
+  final void Function()? onDelete;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Are you sure?'),
+      content: Text('Do you want to delete ${item.name}?'),
+      actions: [
+        TextButton(
+          onPressed: () {
+            context.router.pop();
+          },
+          child: const Text('Cancel'),
+        ),
+        FilledButton(
+          onPressed: () {
+            context.router.pop();
+            onDelete?.call();
+          },
+          child: const Text('Delete'),
+        ),
+      ],
+    );
+  }
+}
