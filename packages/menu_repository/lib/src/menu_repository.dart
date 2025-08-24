3e093e67e1cba878541979d462ec30e5e7df697e
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-24 20:25:16 +0800

    add get menu image method

--- packages/menu_repository/lib/src/menu_repository.dart
+++ packages/menu_repository/lib/src/menu_repository.dart
@@ -18,4 +18,25 @@ class MenuRepository {
   final AppwriteRepository _appwrite;
   final CacheRepository _cache;
 
+  /// Gets image data of a menu item
+  Future<File> getMenuItemImage(String fileId) async {
+    try {
+      final meta = await _appwrite.getFileMetadata(fileId);
+      final fileType = _cache.getExtensionFromMimeType(meta.mimeType);
+
+      if (fileType == null) {
+        throw Exception('Unsupported file type: ${meta.mimeType}');
+      }
+
+      final filename = '$fileId.$fileType';
+      final file = await _cache.readFileFromCache(filename);
+
+      if (file != null) return file;
+
+      final data = await _appwrite.downloadFile(fileId);
+      return _cache.writeFileToCache(filename, data);
+    } on AppwriteException catch (e) {
+      throw ResponseException.fromCode(e.code ?? -1);
+    }
+  }
 }
