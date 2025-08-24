176380f926eaddc8a54eafccfe6a260a471cd263
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-24 20:24:26 +0800

    remove tests

--- packages/cache_repository/test/src/cache_repository_test.dart
+++ packages/cache_repository/test/src/cache_repository_test.dart
@@ -1,13 +1,4 @@
 // Not required for test files
 // ignore_for_file: prefer_const_constructors
 
-import 'package:cache_repository/cache_repository.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group('CacheRepository', () {
-    test('can be instantiated', () {
-      expect(CacheRepository(), isNotNull);
-    });
-  });
-}
+void main() {}
