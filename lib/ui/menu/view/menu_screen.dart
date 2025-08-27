5be68e670782a21195a916809e5f25ae0e489ba7
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-27 16:14:59 +0800

    dart fix

--- lib/ui/menu/view/menu_screen.dart
+++ lib/ui/menu/view/menu_screen.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:menu_repository/menu_repository.dart';
 import 'package:samgyup_serve/bloc/menu/menu_bloc.dart';
 import 'package:samgyup_serve/router/router.dart';
 import 'package:samgyup_serve/ui/components/components.dart';
