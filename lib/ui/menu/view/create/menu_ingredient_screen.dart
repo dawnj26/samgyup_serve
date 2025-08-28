5ff3a84f5d4cf632b060902ff9bc416b30c15ba9
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-28 15:16:27 +0800

    refactor

--- lib/ui/menu/view/create/menu_ingredient_screen.dart
+++ lib/ui/menu/view/create/menu_ingredient_screen.dart
@@ -13,70 +13,87 @@ class MenuIngredientScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return Column(
+    return const Column(
       children: [
         Padding(
-          padding: const EdgeInsets.only(top: 24, left: 16, right: 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Ingredients',
-                style: textTheme.headlineMedium,
-              ),
-              const _AddButton(),
-            ],
-          ),
+          padding: EdgeInsets.only(top: 24, left: 16, right: 16),
+          child: _Header(),
         ),
-        const SizedBox(height: 16),
+        SizedBox(height: 16),
         Expanded(
-          child: BlocBuilder<MenuCreateBloc, MenuCreateState>(
-            buildWhen: (p, c) => p.ingredients != c.ingredients,
-            builder: (context, state) {
-              final ingredients = state.ingredients;
+          child: _IngredientList(),
+        ),
+      ],
+    );
+  }
+}
 
-              if (ingredients.isEmpty) {
-                return const EmptyFallback(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  message: '''
+class _Header extends StatelessWidget {
+  const _Header();
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Ingredients',
+          style: textTheme.headlineMedium,
+        ),
+        const _AddButton(),
+      ],
+    );
+  }
+}
+
+class _IngredientList extends StatelessWidget {
+  const _IngredientList();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MenuCreateBloc, MenuCreateState>(
+      buildWhen: (p, c) => p.ingredients != c.ingredients,
+      builder: (context, state) {
+        final ingredients = state.ingredients;
+
+        if (ingredients.isEmpty) {
+          return const EmptyFallback(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            message: '''
 No ingredients added yet.
 Tap the + button to add ingredients.
                       ''',
-                );
-              }
+          );
+        }
 
-              return ListView.builder(
-                itemBuilder: (ctx, i) {
-                  final ingredient = ingredients[i];
+        return ListView.builder(
+          itemBuilder: (ctx, i) {
+            final ingredient = ingredients[i];
 
-                  return IngredientTile(
-                    ingredient: ingredient,
-                    trailing: IconButton(
-                      onPressed: () {
-                        final updated = ingredients
-                            .where(
-                              (ing) =>
-                                  ing.inventoryItemId !=
-                                  ingredient.inventoryItemId,
-                            )
-                            .toList();
+            return IngredientTile(
+              ingredient: ingredient,
+              trailing: IconButton(
+                onPressed: () {
+                  final updated = ingredients
+                      .where(
+                        (ing) =>
+                            ing.inventoryItemId != ingredient.inventoryItemId,
+                      )
+                      .toList();
 
-                        context.read<MenuCreateBloc>().add(
-                          MenuCreateEvent.ingredientsChanged(updated),
-                        );
-                      },
-                      icon: const Icon(Icons.close_rounded),
-                    ),
+                  context.read<MenuCreateBloc>().add(
+                    MenuCreateEvent.ingredientsChanged(updated),
                   );
                 },
-                itemCount: ingredients.length,
-              );
-            },
-          ),
-        ),
-      ],
+                icon: const Icon(Icons.close_rounded),
+              ),
+            );
+          },
+          itemCount: ingredients.length,
+        );
+      },
     );
   }
 }
