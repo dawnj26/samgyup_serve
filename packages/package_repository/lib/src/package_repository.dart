4da542037a0a63a2c43b7d169d1ea996ae05210e
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-29 20:31:56 +0800

    init appwrite

--- packages/package_repository/lib/src/package_repository.dart
+++ packages/package_repository/lib/src/package_repository.dart
@@ -1,7 +1,13 @@
+import 'package:appwrite_repository/appwrite_repository.dart';
+
 /// {@template package_repository}
 /// Repository for managing packages.
 /// {@endtemplate}
 class PackageRepository {
   /// {@macro package_repository}
-  const PackageRepository();
+  PackageRepository({
+    AppwriteRepository? appwriteRepository,
+  }) : _appwriteRepository = appwriteRepository ?? AppwriteRepository.instance;
+
+  final AppwriteRepository _appwriteRepository;
 }
