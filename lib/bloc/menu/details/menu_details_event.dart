80af9541eae49996ba5f1be5727d12a150f25cca
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-29 16:47:18 +0800

    add reload events

--- lib/bloc/menu/details/menu_details_event.dart
+++ lib/bloc/menu/details/menu_details_event.dart
@@ -3,4 +3,6 @@ part of 'menu_details_bloc.dart';
 @freezed
 class MenuDetailsEvent with _$MenuDetailsEvent {
   const factory MenuDetailsEvent.started() = _Started;
+  const factory MenuDetailsEvent.reloaded() = _Reloaded;
+  const factory MenuDetailsEvent.menuReloaded() = _MenuReloaded;
 }
