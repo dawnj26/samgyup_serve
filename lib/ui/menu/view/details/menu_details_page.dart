8285045b5f483f3bfff4d2165a0fc37553431125
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-08-29 16:48:34 +0800

    provide ingredient edit bloc and listen to state

--- lib/ui/menu/view/details/menu_details_page.dart
+++ lib/ui/menu/view/details/menu_details_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:menu_repository/menu_repository.dart';
+import 'package:samgyup_serve/bloc/ingredient/edit/ingredient_edit_bloc.dart';
 import 'package:samgyup_serve/bloc/menu/delete/menu_delete_bloc.dart';
 import 'package:samgyup_serve/bloc/menu/details/menu_details_bloc.dart';
 import 'package:samgyup_serve/router/router.dart';
@@ -18,24 +19,47 @@ class MenuDetailsPage extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<MenuDeleteBloc, MenuDeleteState>(
-      listener: (context, state) {
-        switch (state) {
-          case MenuDeleteDeleting():
-            showLoadingDialog(
-              context: context,
-              message: 'Deleting menu item...',
-            );
-          case MenuDeleteSuccess():
-            context.router.popUntilRouteWithName(MenuRoute.name);
-            showSnackBar(context, 'Menu item deleted successfully');
-            onChange?.call(needsReload: true);
-          case MenuDeleteError(:final message):
-            context.router.pop();
-            showErrorDialog(context: context, message: message);
-        }
-      },
-      child: const MenuDetailsScreen(),
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<MenuDeleteBloc, MenuDeleteState>(
+          listener: (context, state) {
+            switch (state) {
+              case MenuDeleteDeleting():
+                showLoadingDialog(
+                  context: context,
+                  message: 'Deleting menu item...',
+                );
+              case MenuDeleteSuccess():
+                context.router.popUntilRouteWithName(MenuRoute.name);
+                showSnackBar(context, 'Menu item deleted successfully');
+                onChange?.call(needsReload: true);
+              case MenuDeleteError(:final message):
+                context.router.pop();
+                showErrorDialog(context: context, message: message);
+            }
+          },
+        ),
+        BlocListener<IngredientEditBloc, IngredientEditState>(
+          listener: (context, state) {
+            switch (state) {
+              case IngredientEditSaving():
+                showLoadingDialog(
+                  context: context,
+                  message: 'Saving ingredients...',
+                );
+              case IngredientEditSaved():
+                context.router.pop();
+                context.read<MenuDetailsBloc>().add(
+                  const MenuDetailsEvent.reloaded(),
+                );
+                showSnackBar(context, 'Ingredients saved successfully');
+              case IngredientEditError(:final message):
+                context.router.pop();
+                showErrorDialog(context: context, message: message);
+            }
+          },
+        ),
+      ],
     );
   }
 
@@ -51,6 +75,11 @@ class MenuDetailsPage extends StatelessWidget implements AutoRouteWrapper {
         ),
         BlocProvider(
           create: (context) => MenuDeleteBloc(
+            menuRepository: context.read<MenuRepository>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => IngredientEditBloc(
             menuRepository: context.read<MenuRepository>(),
           ),
         ),
