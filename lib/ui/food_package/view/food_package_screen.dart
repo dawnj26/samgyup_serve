c42689d85560fdfa8a1d376fe14be2424c7ed017
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-01 19:58:30 +0800

    add bloc builders to component to render packages

--- lib/ui/food_package/view/food_package_screen.dart
+++ lib/ui/food_package/view/food_package_screen.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:package_repository/package_repository.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:samgyup_serve/bloc/food_package/food_package_bloc.dart';
 import 'package:samgyup_serve/router/router.dart';
+import 'package:samgyup_serve/ui/components/bottom_loader.dart';
 import 'package:samgyup_serve/ui/food_package/components/components.dart';
 
 class FoodPackageScreen extends StatelessWidget {
@@ -14,37 +16,20 @@ class FoodPackageScreen extends StatelessWidget {
         leading: const AutoLeadingButton(),
         title: const Text('Packages'),
       ),
-      body: CustomScrollView(
+      body: const CustomScrollView(
         slivers: [
-          const SliverToBoxAdapter(
+          SliverToBoxAdapter(
             child: Padding(
               padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
-              child: HomeStatus(count: 10),
+              child: _Status(),
             ),
           ),
           SliverPadding(
-            padding: const EdgeInsets.all(16),
-            sliver: SliverGrid.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                crossAxisCount: 1,
-              ),
-              itemCount: 10,
-              itemBuilder: (ctx, i) {
-                return PackageTile(
-                  onTap: _handleTap,
-                  package: FoodPackage(
-                    name: 'Package 1',
-                    description: 'a description',
-                    price: 200,
-                    timeLimit: 20,
-                    createdAt: DateTime.now(),
-                    menuIds: [],
-                  ),
-                );
-              },
-            ),
+            padding: EdgeInsets.all(16),
+            sliver: _PackageList(),
+          ),
+          SliverToBoxAdapter(
+            child: _Loader(),
           ),
         ],
       ),
@@ -54,6 +39,82 @@ class FoodPackageScreen extends StatelessWidget {
         },
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class _Status extends StatelessWidget {
+  const _Status();
+
+  @override
+  Widget build(BuildContext context) {
+    final total = context.select(
+      (FoodPackageBloc bloc) => bloc.state.totalPackages,
+    );
+
+    return HomeStatus(count: total);
+  }
+}
+
+class _Loader extends StatelessWidget {
+  const _Loader();
+
+  @override
+  Widget build(BuildContext context) {
+    final hasReachedMax = context.select(
+      (FoodPackageBloc bloc) => bloc.state.hasReachedMax,
+    );
+
+    if (hasReachedMax) {
+      return const SizedBox.shrink();
+    }
+
+    return const BottomLoader();
+  }
+}
+
+class _PackageList extends StatelessWidget {
+  const _PackageList();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<FoodPackageBloc, FoodPackageState>(
+      builder: (context, state) {
+        switch (state) {
+          case FoodPackageSuccess(:final packages):
+            return SliverGrid.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 1,
+              ),
+              itemCount: packages.length,
+              itemBuilder: (ctx, i) {
+                final package = packages[i];
+
+                return PackageTile(
+                  onTap: _handleTap,
+                  package: package,
+                );
+              },
+            );
+
+          case FoodPackageFailure():
+            return const SliverFillRemaining(
+              hasScrollBody: false,
+              child: Center(
+                child: Text('Failed to load packages'),
+              ),
+            );
+          default:
+            return const SliverFillRemaining(
+              hasScrollBody: false,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+        }
+      },
     );
   }
 
