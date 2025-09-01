aa13e8e2bb96dbae0fc7aca2ed3385e7e6edece2
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-01 21:05:20 +0800

    export details page

--- lib/ui/food_package/food_package.dart
+++ lib/ui/food_package/food_package.dart
@@ -1,3 +1,4 @@
 export 'shell/food_package_shell_page.dart';
 export 'view/create/food_package_create_page.dart';
+export 'view/details/food_package_details_page.dart';
 export 'view/food_package_page.dart';
