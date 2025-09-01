16ba201bc098ac9859b4829a01a2efd4777faaa0
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-01 17:09:04 +0800

    wrap with package repository

--- lib/ui/food_package/shell/food_package_shell_page.dart
+++ lib/ui/food_package/shell/food_package_shell_page.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:package_repository/package_repository.dart';
 
 @RoutePage()
 class FoodPackageShellPage extends StatelessWidget implements AutoRouteWrapper {
@@ -12,6 +14,9 @@ class FoodPackageShellPage extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget wrappedRoute(BuildContext context) {
-    return this;
+    return RepositoryProvider(
+      create: (context) => PackageRepository(),
+      child: this,
+    );
   }
 }
