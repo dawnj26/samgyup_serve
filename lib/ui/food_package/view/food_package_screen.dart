911572f94359d0af9bc2c24bd85a0d135fb5b6c3
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-01 14:33:06 +0800

    add sample package tiles and status

--- lib/ui/food_package/view/food_package_screen.dart
+++ lib/ui/food_package/view/food_package_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:package_repository/package_repository.dart';
+import 'package:samgyup_serve/ui/food_package/components/components.dart';
 
 class FoodPackageScreen extends StatelessWidget {
   const FoodPackageScreen({super.key});
@@ -9,11 +11,46 @@ class FoodPackageScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: const AutoLeadingButton(),
-        title: const Text('Package Screen'),
+        title: const Text('Packages'),
       ),
-      body: const Center(
-        child: Text('This is the Package Screen'),
+      body: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+              child: HomeStatus(count: 10),
+            ),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.all(16),
+            sliver: SliverGrid.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: 1,
+              ),
+              itemCount: 10,
+              itemBuilder: (ctx, i) {
+                return PackageTile(
+                  onTap: _handleTap,
+                  package: FoodPackage(
+                    name: 'Package 1',
+                    description: 'a description',
+                    price: 200,
+                    timeLimit: 20,
+                    createdAt: DateTime.now(),
+                    menuIds: [],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void _handleTap() {
+    // TODO(package): implement on tap event
   }
 }
