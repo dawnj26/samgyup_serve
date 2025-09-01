16ac193a6e59bb613a8545e3827c0ced4524317c
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-01 17:08:38 +0800

    add food package create bloc

---
+++ lib/bloc/food_package/create/food_package_create_bloc.dart
@@ -0,0 +1,185 @@
+import 'dart:io';
+
+import 'package:appwrite_repository/appwrite_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:formz/formz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:package_repository/package_repository.dart';
+import 'package:samgyup_serve/shared/form/inventory/description.dart';
+import 'package:samgyup_serve/shared/form/name.dart';
+import 'package:samgyup_serve/shared/form/price.dart';
+import 'package:samgyup_serve/shared/form/time_limit.dart';
+
+part 'food_package_create_bloc.freezed.dart';
+part 'food_package_create_event.dart';
+part 'food_package_create_state.dart';
+
+class FoodPackageCreateBloc
+    extends Bloc<FoodPackageCreateEvent, FoodPackageCreateState> {
+  FoodPackageCreateBloc({
+    required PackageRepository packageRepository,
+  }) : _packageRepository = packageRepository,
+       super(const FoodPackageCreateInitial()) {
+    on<_NameChanged>(_onNameChanged);
+    on<_DescriptionChanged>(_onDescriptionChanged);
+    on<_PriceChanged>(_onPriceChanged);
+    on<_TimeLimitChanged>(_onTimeLimitChanged);
+    on<_ImageChanged>(_onImageChanged);
+    on<_Submitted>(_onSubmitted);
+  }
+
+  final PackageRepository _packageRepository;
+
+  void _onNameChanged(
+    _NameChanged event,
+    Emitter<FoodPackageCreateState> emit,
+  ) {
+    final name = Name.dirty(event.name);
+    emit(
+      FoodPackageCreateChanged(
+        name: name,
+        description: state.description,
+        price: state.price,
+        timeLimit: state.timeLimit,
+        image: state.image,
+      ),
+    );
+  }
+
+  void _onDescriptionChanged(
+    _DescriptionChanged event,
+    Emitter<FoodPackageCreateState> emit,
+  ) {
+    final description = Description.dirty(event.description);
+    emit(
+      FoodPackageCreateChanged(
+        name: state.name,
+        description: description,
+        price: state.price,
+        timeLimit: state.timeLimit,
+        image: state.image,
+      ),
+    );
+  }
+
+  void _onPriceChanged(
+    _PriceChanged event,
+    Emitter<FoodPackageCreateState> emit,
+  ) {
+    final price = Price.dirty(event.price);
+    emit(
+      FoodPackageCreateChanged(
+        name: state.name,
+        description: state.description,
+        price: price,
+        timeLimit: state.timeLimit,
+        image: state.image,
+      ),
+    );
+  }
+
+  void _onTimeLimitChanged(
+    _TimeLimitChanged event,
+    Emitter<FoodPackageCreateState> emit,
+  ) {
+    final timeLimit = TimeLimit.dirty(event.timeLimit);
+    emit(
+      FoodPackageCreateChanged(
+        name: state.name,
+        description: state.description,
+        price: state.price,
+        timeLimit: timeLimit,
+        image: state.image,
+      ),
+    );
+  }
+
+  void _onImageChanged(
+    _ImageChanged event,
+    Emitter<FoodPackageCreateState> emit,
+  ) {
+    emit(
+      FoodPackageCreateChanged(
+        name: state.name,
+        description: state.description,
+        price: state.price,
+        timeLimit: state.timeLimit,
+        image: event.image,
+      ),
+    );
+  }
+
+  Future<void> _onSubmitted(
+    _Submitted event,
+    Emitter<FoodPackageCreateState> emit,
+  ) async {
+    final name = Name.dirty(state.name.value);
+    final description = Description.dirty(state.description.value);
+    final price = Price.dirty(state.price.value);
+    final timeLimit = TimeLimit.dirty(state.timeLimit.value);
+
+    final isFormValid = Formz.validate([
+      name,
+      description,
+      price,
+      timeLimit,
+    ]);
+
+    if (!isFormValid) {
+      return emit(
+        FoodPackageCreateChanged(
+          name: name,
+          description: description,
+          price: price,
+          timeLimit: timeLimit,
+          image: state.image,
+        ),
+      );
+    }
+
+    emit(
+      FoodPackageCreateCreating(
+        name: name,
+        description: description,
+        price: price,
+        timeLimit: timeLimit,
+        image: state.image,
+      ),
+    );
+
+    try {
+      final package = FoodPackage(
+        name: name.value,
+        description: description.value,
+        price: double.parse(price.value),
+        timeLimit: int.parse(timeLimit.value),
+        createdAt: DateTime.now(),
+        menuIds: [],
+      );
+      final newPackage = await _packageRepository.createPackage(package);
+      emit(FoodPackageCreateSuccess(foodPackage: newPackage));
+    } on ResponseException catch (e) {
+      emit(
+        FoodPackageCreateFailure(
+          errorMessage: e.message,
+          name: name,
+          description: description,
+          price: price,
+          timeLimit: timeLimit,
+          image: state.image,
+        ),
+      );
+    } on Exception catch (e) {
+      emit(
+        FoodPackageCreateFailure(
+          errorMessage: e.toString(),
+          name: name,
+          description: description,
+          price: price,
+          timeLimit: timeLimit,
+          image: state.image,
+        ),
+      );
+    }
+  }
+}
---
+++ lib/bloc/food_package/create/food_package_create_event.dart
@@ -0,0 +1,15 @@
+part of 'food_package_create_bloc.dart';
+
+@freezed
+class FoodPackageCreateEvent with _$FoodPackageCreateEvent {
+  const factory FoodPackageCreateEvent.nameChanged(String name) = _NameChanged;
+  const factory FoodPackageCreateEvent.descriptionChanged(String description) =
+      _DescriptionChanged;
+  const factory FoodPackageCreateEvent.priceChanged(String price) =
+      _PriceChanged;
+  const factory FoodPackageCreateEvent.timeLimitChanged(String timeLimit) =
+      _TimeLimitChanged;
+  const factory FoodPackageCreateEvent.imageChanged(File? image) =
+      _ImageChanged;
+  const factory FoodPackageCreateEvent.submitted() = _Submitted;
+}
