5eb881507159ae2406e44e89400ee9228a279639
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-02 16:58:11 +0800

    add refreshed event

--- lib/bloc/food_package/details/food_package_details_event.dart
+++ lib/bloc/food_package/details/food_package_details_event.dart
@@ -3,4 +3,5 @@ part of 'food_package_details_bloc.dart';
 @freezed
 class FoodPackageDetailsEvent with _$FoodPackageDetailsEvent {
   const factory FoodPackageDetailsEvent.started() = _Started;
+  const factory FoodPackageDetailsEvent.refreshed() = _Refreshed;
 }
