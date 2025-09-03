9515251669be60454002b0e3b2f95e07c9ec7cb4
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-03 16:18:25 +0800

    export edit page

--- lib/ui/food_package/food_package.dart
+++ lib/ui/food_package/food_package.dart
@@ -1,5 +1,6 @@
 export 'shell/food_package_shell_page.dart';
 export 'view/create/food_package_create_page.dart';
 export 'view/details/food_package_details_page.dart';
+export 'view/edit/food_package_edit_page.dart';
 export 'view/food_package_page.dart';
 export 'view/menu/menu_select_page.dart';
