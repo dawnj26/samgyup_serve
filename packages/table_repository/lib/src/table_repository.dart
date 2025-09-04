db2217012e501038e6809a96312f52ce828b4076
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-04 18:53:47 +0800

    add table repository

---
+++ packages/table_repository/lib/src/table_repository.dart
@@ -0,0 +1,7 @@
+/// {@template table_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class TableRepository {
+  /// {@macro table_repository}
+  const TableRepository();
+}
