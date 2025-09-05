34d3b39c13b51a5a68455d90992b66911c9ba81a
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-05 18:52:40 +0800

    add padding and support fullscreen

--- lib/ui/components/bottom_sheet_layout.dart
+++ lib/ui/components/bottom_sheet_layout.dart
@@ -2,16 +2,43 @@ import 'package:flutter/material.dart';
 
 class BottomSheetLayout extends StatelessWidget {
   const BottomSheetLayout({
-    required this.height,
     required this.child,
+    this.height = 0,
     super.key,
-  });
+    this.fullscreen = false,
+    this.padding,
+  }) : assert(
+         height > 0 || fullscreen,
+         'Height must be greater than 0 if not fullscreen',
+       );
 
   final double height;
   final Widget child;
+  final bool fullscreen;
+  final EdgeInsetsGeometry? padding;
 
   @override
   Widget build(BuildContext context) {
+    if (fullscreen) {
+      return Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Icon(Icons.drag_handle_rounded)],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: padding ?? EdgeInsets.zero,
+              child: child,
+            ),
+          ),
+        ],
+      );
+    }
+
     return SizedBox(
       height: height,
       child: Column(
@@ -23,7 +50,12 @@ class BottomSheetLayout extends StatelessWidget {
               children: [Icon(Icons.drag_handle_rounded)],
             ),
           ),
-          Expanded(child: child),
+          Expanded(
+            child: Padding(
+              padding: padding ?? EdgeInsets.zero,
+              child: child,
+            ),
+          ),
         ],
       ),
     );
