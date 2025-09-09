867c4797e91c1ef17d034bee8482015d6b5ab179
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-09 19:50:47 +0800

    export components

--- lib/ui/table/components/components.dart
+++ lib/ui/table/components/components.dart
@@ -1,5 +1,7 @@
 export 'capacity_input.dart';
-export 'create_table_bottom_sheet.dart';
+export 'table_capacity.dart';
+export 'table_form_bottom_sheet.dart';
+export 'table_more_option_button.dart';
 export 'table_number_input.dart';
 export 'table_status_badge.dart';
 export 'table_status_dropdown_menu.dart';
