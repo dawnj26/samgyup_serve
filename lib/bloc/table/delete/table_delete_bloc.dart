a75825705ac42c83ef60b12edfd5a38182bffbe8
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-11 20:17:01 +0800

    remove device with table id

--- lib/bloc/table/delete/table_delete_bloc.dart
+++ lib/bloc/table/delete/table_delete_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:appwrite_repository/appwrite_repository.dart';
 import 'package:bloc/bloc.dart';
+import 'package:device_repository/device_repository.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:table_repository/table_repository.dart';
 
@@ -8,13 +9,17 @@ part 'table_delete_state.dart';
 part 'table_delete_bloc.freezed.dart';
 
 class TableDeleteBloc extends Bloc<TableDeleteEvent, TableDeleteState> {
-  TableDeleteBloc({required TableRepository tableRepository})
-    : _repo = tableRepository,
-      super(const _Initial()) {
+  TableDeleteBloc({
+    required TableRepository tableRepository,
+    required DeviceRepository deviceRepository,
+  }) : _repo = tableRepository,
+       _deviceRepo = deviceRepository,
+       super(const _Initial()) {
     on<_Started>(_onStarted);
   }
 
   final TableRepository _repo;
+  final DeviceRepository _deviceRepo;
 
   Future<void> _onStarted(
     _Started event,
@@ -23,6 +28,13 @@ class TableDeleteBloc extends Bloc<TableDeleteEvent, TableDeleteState> {
     emit(state.copyWith(status: TableDeleteStatus.loading));
     try {
       await _repo.deleteTable(event.id);
+      final device = await _deviceRepo.getDeviceByTable(event.id);
+      if (device != null) {
+        await _deviceRepo.updateDevice(
+          device.copyWith(tableId: null),
+        );
+      }
+
       emit(state.copyWith(status: TableDeleteStatus.success));
     } on ResponseException catch (e) {
       emit(
