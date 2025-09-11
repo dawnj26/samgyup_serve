402218d12c4a191c3a14564c4c0132541435a187
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-11 20:16:48 +0800

    include assigned tables

--- lib/bloc/device/select/device_select_bloc.dart
+++ lib/bloc/device/select/device_select_bloc.dart
@@ -2,6 +2,7 @@ import 'package:appwrite_repository/appwrite_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:device_repository/device_repository.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:table_repository/table_repository.dart';
 
 part 'device_select_event.dart';
 part 'device_select_state.dart';
@@ -10,13 +11,16 @@ part 'device_select_bloc.freezed.dart';
 class DeviceSelectBloc extends Bloc<DeviceSelectEvent, DeviceSelectState> {
   DeviceSelectBloc({
     required DeviceRepository deviceRepository,
-  }) : _repo = deviceRepository,
+    required TableRepository tableRepository,
+  }) : _deviceRepo = deviceRepository,
+       _tableRepo = tableRepository,
        super(const _Initial()) {
     on<_Started>(_onStarted);
     on<_LoadMore>(_onLoadMore);
   }
 
-  final DeviceRepository _repo;
+  final DeviceRepository _deviceRepo;
+  final TableRepository _tableRepo;
   final int _pageSize = 20;
 
   Future<void> _onStarted(
@@ -25,12 +29,15 @@ class DeviceSelectBloc extends Bloc<DeviceSelectEvent, DeviceSelectState> {
   ) async {
     emit(state.copyWith(status: DeviceSelectStatus.loading));
     try {
-      final devices = await _repo.getAllDevices();
+      final devices = await _deviceRepo.getAllDevices();
+      final tables = await _getTables(devices);
+
       emit(
         state.copyWith(
           status: DeviceSelectStatus.success,
           devices: devices,
           hasReachedMax: devices.length < _pageSize,
+          tables: tables,
         ),
       );
     } on ResponseException catch (e) {
@@ -59,13 +66,17 @@ class DeviceSelectBloc extends Bloc<DeviceSelectEvent, DeviceSelectState> {
     }
 
     try {
-      final devices = await _repo.getAllDevices(
+      final devices = await _deviceRepo.getAllDevices(
         cursor: state.devices.isNotEmpty ? state.devices.last.id : null,
       );
+
+      final tables = await _getTables(devices);
+
       emit(
         state.copyWith(
           status: DeviceSelectStatus.success,
           devices: [...state.devices, ...devices],
+          tables: {...state.tables, ...tables},
           hasReachedMax: devices.length < _pageSize,
         ),
       );
@@ -84,5 +95,16 @@ class DeviceSelectBloc extends Bloc<DeviceSelectEvent, DeviceSelectState> {
         ),
       );
     }
+  }
+
+  Future<Map<String, Table>> _getTables(List<Device> devices) async {
+    final tables = <String, Table>{};
+    for (final device in devices) {
+      if (device.tableId == null) continue;
+
+      tables[device.id] = await _tableRepo.fetchTable(device.tableId!);
+    }
+
+    return tables;
   }
 }
--- lib/bloc/device/select/device_select_state.dart
+++ lib/bloc/device/select/device_select_state.dart
@@ -7,6 +7,7 @@ abstract class DeviceSelectState with _$DeviceSelectState {
   const factory DeviceSelectState.initial({
     @Default(DeviceSelectStatus.initial) DeviceSelectStatus status,
     @Default([]) List<Device> devices,
+    @Default({}) Map<String, Table> tables,
     @Default(false) bool hasReachedMax,
     String? errorMessage,
   }) = _Initial;
