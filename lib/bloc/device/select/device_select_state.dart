1e22d9b5069d158e095d4e253944d09fb102b576
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-11 15:25:10 +0800

    add device select bloc for loading devices

---
+++ lib/bloc/device/select/device_select_state.dart
@@ -0,0 +1,13 @@
+part of 'device_select_bloc.dart';
+
+enum DeviceSelectStatus { initial, loading, success, failure }
+
+@freezed
+abstract class DeviceSelectState with _$DeviceSelectState {
+  const factory DeviceSelectState.initial({
+    @Default(DeviceSelectStatus.initial) DeviceSelectStatus status,
+    @Default([]) List<Device> devices,
+    @Default(false) bool hasReachedMax,
+    String? errorMessage,
+  }) = _Initial;
+}
