1a45d602501aa3fbfe228b8b1a9c0f9f1385f125
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-11 20:17:17 +0800

    add assigning state and assign event

--- lib/bloc/table/details/table_details_event.dart
+++ lib/bloc/table/details/table_details_event.dart
@@ -4,4 +4,5 @@ part of 'table_details_bloc.dart';
 class TableDetailsEvent with _$TableDetailsEvent {
   const factory TableDetailsEvent.started() = _Started;
   const factory TableDetailsEvent.changed() = _Changed;
+  const factory TableDetailsEvent.assigned(Device device) = _Assigned;
 }
--- lib/bloc/table/details/table_details_state.dart
+++ lib/bloc/table/details/table_details_state.dart
@@ -7,11 +7,20 @@ enum TableDetailsStatus {
   failure,
 }
 
+enum TableAssignmentStatus {
+  initial,
+  assigning,
+  success,
+  failure,
+}
+
 @freezed
 abstract class TableDetailsState with _$TableDetailsState {
   const factory TableDetailsState.initial({
     required Table table,
     @Default(false) bool isDirty,
+    @Default(TableAssignmentStatus.initial)
+    TableAssignmentStatus assignmentStatus,
     @Default(TableDetailsStatus.initial) TableDetailsStatus status,
     Device? device,
     String? errorMessage,
