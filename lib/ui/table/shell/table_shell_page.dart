7ad88d33198254fb963e304dcf02b92f398f3af9
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-11 20:19:22 +0800

    remove wrapper

--- lib/ui/table/shell/table_shell_page.dart
+++ lib/ui/table/shell/table_shell_page.dart
@@ -1,22 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:table_repository/table_repository.dart';
 
 @RoutePage()
-class TableShellPage extends StatelessWidget implements AutoRouteWrapper {
+class TableShellPage extends StatelessWidget {
   const TableShellPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const AutoRouter();
-  }
-
-  @override
-  Widget wrappedRoute(BuildContext context) {
-    return RepositoryProvider(
-      create: (context) => TableRepository(),
-      child: this,
-    );
   }
 }
