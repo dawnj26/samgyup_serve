3acf1df26e1d7a312d2a020716876d48d175de84
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-12 19:14:00 +0800

    add menu and package tabs

--- lib/ui/order/view/order_screen.dart
+++ lib/ui/order/view/order_screen.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:menu_repository/menu_repository.dart';
+import 'package:samgyup_serve/ui/components/app_logo_icon.dart';
+import 'package:samgyup_serve/ui/order/view/tabs/menu_tab.dart';
+import 'package:samgyup_serve/ui/order/view/tabs/package_tab.dart';
 
-class OrderScreen extends StatelessWidget {
+class OrderScreen extends StatefulWidget {
   const OrderScreen({super.key});
+
+  @override
+  State<OrderScreen> createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+  late final List<Widget> _tabs;
+  late final List<Widget> _tabViews;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabs = [
+      const Tab(text: 'Unli Packages'),
+      ...MenuCategory.values.map((c) => Tab(text: c.label)),
+    ];
+    _tabViews = [
+      const PackageTab(),
+      ...MenuCategory.values.map((c) => MenuTab(category: c)),
+    ];
+
+    _tabController = TabController(length: _tabs.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Order Screen'),
+        leading: const AppLogoIcon(
+          size: 40,
+          padding: EdgeInsets.all(8),
+        ),
+        title: const Text('Order'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: _tabs,
+          isScrollable: true,
+        ),
       ),
-      body: const Center(
-        child: Text('This is the Order Screen'),
+      body: TabBarView(
+        controller: _tabController,
+        children: _tabViews,
       ),
     );
   }
