0b2d5ac5b88dd1e904b3955598bfd2eeae2f3307
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-12 19:13:13 +0800

    add optional padding

--- lib/ui/components/app_logo_icon.dart
+++ lib/ui/components/app_logo_icon.dart
@@ -9,23 +9,26 @@ class AppLogoIcon extends StatelessWidget {
   const AppLogoIcon({
     this.size = 32,
     this.variant = AppLogoIconVariant.color,
+    this.padding = EdgeInsets.zero,
     super.key,
   });
+
   final double size;
   final AppLogoIconVariant variant;
+  final EdgeInsetsGeometry padding;
 
   @override
   Widget build(BuildContext context) {
-    if (variant == AppLogoIconVariant.blackAndWhite) {
-      return Image(
-        image: const AssetImage('assets/images/logo_icon_bw.png'),
-        width: size,
-      );
-    }
+    final assetPath = variant == AppLogoIconVariant.color
+        ? 'assets/images/logo_icon.png'
+        : 'assets/images/logo_icon_bw.png';
 
-    return Image(
-      image: const AssetImage('assets/images/logo_icon.png'),
-      width: size,
+    return Padding(
+      padding: padding,
+      child: Image(
+        image: AssetImage(assetPath),
+        width: size,
+      ),
     );
   }
 }
