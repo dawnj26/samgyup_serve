544d331782a73f68c8a686d8b1ac9f8100299500
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-12 20:39:20 +0800

    provide activity bloc

--- lib/ui/home/shell/home_shell_page.dart
+++ lib/ui/home/shell/home_shell_page.dart
@@ -1,6 +1,17 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:samgyup_serve/bloc/activity/activity_bloc.dart';
 
 @RoutePage()
-class HomeShellPage extends AutoRouter {
+class HomeShellPage extends AutoRouter implements AutoRouteWrapper {
   const HomeShellPage({super.key});
+
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    return BlocProvider(
+      create: (context) => ActivityBloc(),
+      child: this,
+    );
+  }
 }
