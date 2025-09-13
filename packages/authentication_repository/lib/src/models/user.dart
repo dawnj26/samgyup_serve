6561036a6a46a09b94dc7ff84ead9c17d85712b9
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-13 14:58:34 +0800

    add isGuest checker

--- packages/authentication_repository/lib/src/models/user.dart
+++ packages/authentication_repository/lib/src/models/user.dart
@@ -15,9 +15,15 @@ abstract class User with _$User {
     String? name,
   }) = _User;
 
+  const User._();
+
   /// Creates an empty User instance.
   factory User.empty() => User(id: '');
 
   /// Converts a JSON map to a User instance.
   factory User.fromJson(Map<String, Object?> json) => _$UserFromJson(json);
+
+  /// Indicates whether the user is a guest (not logged in).
+  bool get isGuest =>
+      (email == null || email!.isEmpty) && (name == null || name!.isEmpty);
 }
