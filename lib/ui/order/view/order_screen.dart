4e21e41b96f5ab437e51f60d16452ddc608fd70e
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-14 15:32:49 +0800

    add view cart button

--- lib/ui/order/view/order_screen.dart
+++ lib/ui/order/view/order_screen.dart
@@ -1,7 +1,10 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:menu_repository/menu_repository.dart';
+import 'package:samgyup_serve/bloc/order/cart/order_cart_bloc.dart';
 import 'package:samgyup_serve/bloc/order/order_bloc.dart';
+import 'package:samgyup_serve/router/router.dart';
 import 'package:samgyup_serve/ui/components/app_logo_icon.dart';
 import 'package:samgyup_serve/ui/order/view/tabs/menu_tab.dart';
 import 'package:samgyup_serve/ui/order/view/tabs/package_tab.dart';
@@ -74,6 +77,33 @@ class _OrderScreenState extends State<OrderScreen>
           );
         },
       ),
+      bottomNavigationBar: const Padding(
+        padding: EdgeInsets.all(16),
+        child: _ViewCartButton(),
+      ),
+    );
+  }
+}
+
+class _ViewCartButton extends StatelessWidget {
+  const _ViewCartButton();
+
+  @override
+  Widget build(BuildContext context) {
+    final menuCart = context.select(
+      (OrderCartBloc bloc) => bloc.state.menuItems,
+    );
+    final packageCart = context.select(
+      (OrderCartBloc bloc) => bloc.state.packages,
+    );
+
+    final cartCount = menuCart.length + packageCart.length;
+
+    return FilledButton(
+      onPressed: () {
+        context.router.push(const OrderCartRoute());
+      },
+      child: Text('View Cart ($cartCount)'),
     );
   }
 }
