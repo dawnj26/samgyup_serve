ea3522a203d2fa3b02983f4e7c7bf7ced2a72475
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-15 14:22:46 +0800

    remove order bloc and table repo providers

--- lib/ui/order/shell/order_shell_page.dart
+++ lib/ui/order/shell/order_shell_page.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:menu_repository/menu_repository.dart';
 import 'package:package_repository/package_repository.dart';
-import 'package:samgyup_serve/bloc/app/app_bloc.dart';
 import 'package:samgyup_serve/bloc/order/cart/order_cart_bloc.dart';
-import 'package:samgyup_serve/bloc/order/order_bloc.dart';
-import 'package:table_repository/table_repository.dart';
 
 @RoutePage()
 class OrderShellPage extends AutoRouter implements AutoRouteWrapper {
@@ -22,18 +19,9 @@ class OrderShellPage extends AutoRouter implements AutoRouteWrapper {
         RepositoryProvider(
           create: (context) => PackageRepository(),
         ),
-        RepositoryProvider(
-          create: (context) => TableRepository(),
-        ),
       ],
       child: MultiBlocProvider(
         providers: [
-          BlocProvider(
-            create: (context) => OrderBloc(
-              tableRepository: context.read<TableRepository>(),
-              tableId: context.read<AppBloc>().state.device!.tableId!,
-            )..add(const OrderEvent.started()),
-          ),
           BlocProvider(
             create: (context) => OrderCartBloc(),
           ),
