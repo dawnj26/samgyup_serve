d7c1430d335fe56dab1b60b92199f1225317dde5
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-16 19:35:33 +0800

    add order start event

--- lib/bloc/order/order_state.dart
+++ lib/bloc/order/order_state.dart
@@ -4,5 +4,9 @@ enum OrderStatus { initial, loading, success, failure }
 
 @freezed
 abstract class OrderState with _$OrderState {
-  const factory OrderState.initial() = _Initial;
+  const factory OrderState.initial({
+    @Default(OrderStatus.initial) OrderStatus status,
+    @Default('') String reservationId,
+    String? errorMessage,
+  }) = _Initial;
 }
