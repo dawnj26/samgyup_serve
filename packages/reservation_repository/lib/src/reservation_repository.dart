6c6bb94617101d395c82e83f3f9760bec01314a8
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-17 17:59:05 +0800

    add getCurrentReservation method

--- packages/reservation_repository/lib/src/reservation_repository.dart
+++ packages/reservation_repository/lib/src/reservation_repository.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:appwrite/appwrite.dart';
 import 'package:appwrite_repository/appwrite_repository.dart';
+import 'package:reservation_repository/src/enums/enums.dart';
 import 'package:reservation_repository/src/models/models.dart';
 
 /// {@template reservation_repository}
@@ -42,6 +43,46 @@ class ReservationRepository {
       return Reservation.fromJson(_appwrite.rowToJson(doc));
     } on AppwriteException catch (e) {
       log(e.toString(), name: 'ReservationRepository.createReservation');
+      throw ResponseException.fromCode(e.code ?? 500);
+    }
+  }
+
+  /// Retrieves the current active reservation for a given table.
+  Future<Reservation?> getCurrentReservation(String tableId) async {
+    try {
+      final result = await _appwrite.databases.listRows(
+        databaseId: _databaseId,
+        tableId: _collectionId,
+        queries: [
+          Query.equal('tableId', tableId),
+          Query.equal('status', ReservationStatus.active.name),
+        ],
+      );
+
+      if (result.total == 0) {
+        return null;
+      }
+
+      final doc = result.rows.first;
+      return Reservation.fromJson(_appwrite.rowToJson(doc));
+    } on AppwriteException catch (e) {
+      log(e.toString(), name: 'ReservationRepository.getCurrentReservation');
+      throw ResponseException.fromCode(e.code ?? 500);
+    }
+  }
+
+  /// Retrieves a reservation by its ID.
+  Future<Reservation> getReservationById(String reservationId) async {
+    try {
+      final doc = await _appwrite.databases.getRow(
+        databaseId: _databaseId,
+        tableId: _collectionId,
+        rowId: reservationId,
+      );
+
+      return Reservation.fromJson(_appwrite.rowToJson(doc));
+    } on AppwriteException catch (e) {
+      log(e.toString(), name: 'ReservationRepository.getReservationById');
       throw ResponseException.fromCode(e.code ?? 500);
     }
   }
