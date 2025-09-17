c9db3b7c1cc4ffe7c4a698da151ca5f4e8d1b2da
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-17 17:57:50 +0800

    add blocs needed for reservation

--- lib/ui/reservation/view/order/reservation_order_page.dart
+++ lib/ui/reservation/view/order/reservation_order_page.dart
@@ -1,5 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:samgyup_serve/bloc/order/list/order_list_bloc.dart';
+import 'package:samgyup_serve/bloc/reservation/reservation_bloc.dart';
 import 'package:samgyup_serve/ui/reservation/view/order/reservation_order_screen.dart';
 
 @RoutePage()
@@ -10,11 +13,41 @@ class ReservationOrderPage extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget build(BuildContext context) {
-    return const ReservationOrderScreen();
+    return BlocListener<ReservationBloc, ReservationState>(
+      listener: (context, state) {
+        if (state.status == ReservationStatus.success) {
+          context.read<OrderListBloc>().add(
+            OrderListEvent.started(orderIds: state.invoice.orderIds),
+          );
+        }
+      },
+      child: const ReservationOrderScreen(),
+    );
   }
 
   @override
   Widget wrappedRoute(BuildContext context) {
-    return this;
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              ReservationBloc(
+                tableRepository: context.read(),
+                reservationRepository: context.read(),
+                billingRepository: context.read(),
+              )..add(
+                ReservationEvent.started(reservationId: reservationId),
+              ),
+        ),
+        BlocProvider(
+          create: (context) => OrderListBloc(
+            orderRepository: context.read(),
+            menuRepository: context.read(),
+            packageRepository: context.read(),
+          ),
+        ),
+      ],
+      child: this,
+    );
   }
 }
