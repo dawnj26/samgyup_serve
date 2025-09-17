ac331392d75619eefe323545ba497e45f1f365b0
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-17 17:52:17 +0800

    register ReservationCreated event

--- lib/bloc/home/home_bloc.dart
+++ lib/bloc/home/home_bloc.dart
@@ -8,6 +8,22 @@ part 'home_bloc.freezed.dart';
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(const _Initial()) {
     on<_StatusChanged>(_onStatusChanged);
+    on<_Started>(_onStarted);
+    on<_ReservationCreated>(_onReservationCreated);
+  }
+
+  final ReservationRepository _reservationRepo;
+
+  Future<void> _onReservationCreated(
+    _ReservationCreated event,
+    Emitter<HomeState> emit,
+  ) async {
+    emit(
+      state.copyWith(
+        session: SessionStatus.reservation,
+        reservationId: event.reservationId,
+      ),
+    );
   }
 
   Future<void> _onStatusChanged(
