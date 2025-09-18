b4b8ab8876c031fc2a63fafeb0166bbd6516b182
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-18 16:26:30 +0800

    export add order page

--- lib/ui/reservation/reservation.dart
+++ lib/ui/reservation/reservation.dart
@@ -1,2 +1,4 @@
 export 'shell/reservation_shell_page.dart';
+export 'view/add_order/reservation_add_order_page.dart';
+export 'view/add_order/reservation_add_order_wrapper_page.dart';
 export 'view/order/reservation_order_page.dart';
