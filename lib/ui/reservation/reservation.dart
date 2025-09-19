395a9c8c8538f3b134a5d509767df924338c8f2e
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-19 17:59:30 +0800

    export billing page

--- lib/ui/reservation/reservation.dart
+++ lib/ui/reservation/reservation.dart
@@ -1,5 +1,6 @@
 export 'shell/reservation_shell_page.dart';
 export 'view/add_order/reservation_add_order_page.dart';
 export 'view/add_order/reservation_add_order_wrapper_page.dart';
+export 'view/billing/reservation_billing_page.dart';
 export 'view/order/reservation_order_page.dart';
 export 'view/refill/reservation_refill_page.dart';
