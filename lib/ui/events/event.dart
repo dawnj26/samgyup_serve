1e7d73e03bf9dc7c0bea8a1f0563ab96de358b39
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-21 19:53:43 +0800

    export payment page

--- lib/ui/events/event.dart
+++ lib/ui/events/event.dart
@@ -2,4 +2,5 @@ export 'shell/event_details_shell_page.dart';
 export 'shell/event_shell_page.dart';
 export 'view/event_page.dart';
 export 'view/order/event_order_page.dart';
+export 'view/payment/event_payment_page.dart';
 export 'view/refill/event_refill_page.dart';
