05c1a5470ca78420da1066bf662d9aa967928415
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-22 15:53:48 +0800

    check if the user has time to still request menus

--- lib/bloc/reservation/refill/reservation_refill_bloc.dart
+++ lib/bloc/reservation/refill/reservation_refill_bloc.dart
@@ -3,6 +3,7 @@ import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:menu_repository/menu_repository.dart';
 import 'package:order_repository/order_repository.dart';
+import 'package:package_repository/package_repository.dart';
 
 part 'reservation_refill_event.dart';
 part 'reservation_refill_state.dart';
@@ -12,17 +13,31 @@ class ReservationRefillBloc
     extends Bloc<ReservationRefillEvent, ReservationRefillState> {
   ReservationRefillBloc({
     required MenuRepository menuRepository,
+    required FoodPackage package,
+    required DateTime startTime,
   }) : _menuRepository = menuRepository,
+       _startTime = startTime,
+       _package = package,
        super(const _Initial()) {
     on<_Started>(_onStarted);
   }
 
   final MenuRepository _menuRepository;
+  final FoodPackage _package;
+  final DateTime _startTime;
 
   Future<void> _onStarted(
     _Started event,
     Emitter<ReservationRefillState> emit,
   ) async {
+    final endTime = _startTime.add(Duration(minutes: _package.timeLimit));
+    final diff = endTime.difference(DateTime.now());
+
+    if (diff.isNegative) {
+      emit(state.copyWith(status: ReservationRefillStatus.timeLimitExceeded));
+      return;
+    }
+
     emit(state.copyWith(status: ReservationRefillStatus.loading));
     try {
       for (final cartItem in event.cartItems) {
