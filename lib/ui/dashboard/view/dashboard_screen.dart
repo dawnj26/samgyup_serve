95860a4f3b352a2e83519a25ed22cbd971affd9c
Donn Jayson S. Quinto <jaysonquinto1@outlook.com>
2025-09-24 16:30:48 +0800

    add revenue summary

--- lib/ui/dashboard/view/dashboard_screen.dart
+++ lib/ui/dashboard/view/dashboard_screen.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:samgyup_serve/ui/dashboard/components/components.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final textTheme = Theme.of(context).textTheme;
+
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(16),
+        padding: const EdgeInsets.all(16),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Welcome to the Dashboard',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+              'Dashboard',
+              style: textTheme.headlineMedium,
             ),
+            const SizedBox(height: 16),
+            const RevenueSummary(),
           ],
         ),
       ),
