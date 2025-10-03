6d74024bae300e9432533d114c9d7ee7ecaa2ad6
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-10-03 17:31:59 +0800

    add qr dialog and picker

---
+++ lib/ui/settings/components/qr_picker.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:samgyup_serve/ui/components/image_picker.dart';
+
+class QrPicker extends StatefulWidget {
+  const QrPicker({super.key});
+
+  @override
+  State<QrPicker> createState() => _QrPickerState();
+}
+
+class _QrPickerState extends State<QrPicker> {
+  File? _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Pick QR Code Image'),
+      content: ImagePicker(
+        onChange: (image) => setState(() => _image = image),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: _image == null
+              ? null
+              : () => Navigator.of(context).pop(_image),
+          child: const Text('Select'),
+        ),
+      ],
+    );
+  }
+}
