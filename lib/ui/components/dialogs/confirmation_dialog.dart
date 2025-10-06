5cf38e0e95ba108185293bb5cae487d43f29d300
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-10-07 00:56:36 +0800

    add callbacks

--- lib/ui/components/dialogs/confirmation_dialog.dart
+++ lib/ui/components/dialogs/confirmation_dialog.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
 class ConfirmationDialog extends StatelessWidget {
-  const ConfirmationDialog({super.key, this.title, this.message});
+  const ConfirmationDialog({
+    super.key,
+    this.title,
+    this.message,
+    this.onConfirmed,
+    this.onCancelled,
+  });
 
   final String? title;
   final String? message;
+  final void Function()? onConfirmed;
+  final void Function()? onCancelled;
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +21,17 @@ class ConfirmationDialog extends StatelessWidget {
       content: Text(message ?? 'This action cannot be undone.'),
       actions: [
         TextButton(
-          onPressed: () => Navigator.of(context).pop(false),
+          onPressed: () {
+            Navigator.of(context).pop(false);
+            onCancelled?.call();
+          },
           child: const Text('Cancel'),
         ),
         TextButton(
-          onPressed: () => Navigator.of(context).pop(true),
+          onPressed: () {
+            Navigator.of(context).pop(true);
+            onConfirmed?.call();
+          },
           child: const Text('Confirm'),
         ),
       ],
