7e0ffacc37d92f6f1687b466de0d309bd64cedaa
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-10-29 16:09:47 +0800

    add more option for batch

---
+++ lib/ui/inventory/components/batch_more_option_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+enum BatchOption {
+  remove;
+
+  String get label {
+    switch (this) {
+      case BatchOption.remove:
+        return 'Remove';
+    }
+  }
+}
+
+class BatchMoreOptionButton extends StatelessWidget {
+  const BatchMoreOptionButton({super.key, this.onSelected});
+
+  final void Function(BatchOption option)? onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<BatchOption>(
+      position: PopupMenuPosition.under,
+      icon: const Icon(Icons.more_vert),
+      onSelected: onSelected,
+      itemBuilder: (context) {
+        return BatchOption.values.map((option) {
+          return PopupMenuItem<BatchOption>(
+            value: option,
+            child: Text(option.label),
+          );
+        }).toList();
+      },
+    );
+  }
+}
