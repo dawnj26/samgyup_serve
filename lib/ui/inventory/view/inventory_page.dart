6e0afb8511e9c75fb40f7c16b34f7b82b54302a7
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-10-30 16:47:28 +0800

    use wrapper and add listener

--- lib/ui/inventory/view/inventory_page.dart
+++ lib/ui/inventory/view/inventory_page.dart
@@ -1,20 +1,39 @@
-import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:samgyup_serve/bloc/inventory/inventory_bloc.dart';
+import 'package:samgyup_serve/shared/dialog.dart';
+import 'package:samgyup_serve/shared/snackbar.dart';
 import 'package:samgyup_serve/ui/inventory/view/inventory_screen.dart';
 
 @RoutePage()
-class InventoryPage extends StatelessWidget {
+class InventoryPage extends StatelessWidget implements AutoRouteWrapper {
   const InventoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          InventoryBloc(inventoryRepository: context.read())
-            ..add(const InventoryEvent.started()),
+    return BlocListener<InventoryBloc, InventoryState>(
+      listener: (context, state) {
+        if (state is InventorySyncing) {
+          showLoadingDialog(context: context, message: 'Syncing inventory...');
+        }
+
+        if (state is InventorySynced) {
+          context.router.pop();
+          showSnackBar(context, 'Synced successfully');
+        }
+      },
       child: const InventoryScreen(),
+    );
+  }
+
+  @override
+  Widget wrappedRoute(BuildContext context) {
+    return BlocProvider(
+      create: (context) => InventoryBloc(
+        inventoryRepository: context.read(),
+      )..add(const InventoryEvent.started()),
+      child: this,
     );
   }
 }
