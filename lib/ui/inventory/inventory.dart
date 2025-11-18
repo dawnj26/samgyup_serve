a8bb2c85183e111cee38f1d1698dc6da532009bc
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-11-18 20:57:13 +0800

    export select page

--- lib/ui/inventory/inventory.dart
+++ lib/ui/inventory/inventory.dart
@@ -5,5 +5,6 @@ export 'view/edit/inventory_edit_page.dart';
 export 'view/inventory_page.dart';
 export 'view/list/category/inventory_category_list_page.dart';
 export 'view/list/status/inventory_status_list_page.dart';
+export 'view/select/inventory_select_page.dart';
 export 'view/stock/add_stock_page.dart';
 export 'view/subcategories/subcategories_page.dart';
