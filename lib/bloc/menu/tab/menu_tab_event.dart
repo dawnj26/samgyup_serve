13732732adfbb6fef279bf49c05f8f8056b6f3c7
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-11-19 17:38:08 +0800

    remove menu tab bloc

--- lib/bloc/menu/tab/menu_tab_event.dart
+++
@@ -1,8 +0,0 @@
-part of 'menu_tab_bloc.dart';
-
-@freezed
-class MenuTabEvent with _$MenuTabEvent {
-  const factory MenuTabEvent.started() = _Started;
-  const factory MenuTabEvent.fetchMore() = _FetchMore;
-  const factory MenuTabEvent.refresh() = _Refresh;
-}
