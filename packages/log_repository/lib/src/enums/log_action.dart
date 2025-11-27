d60713dee1d004e4658301ae244b1feacaeb360d
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-11-28 04:24:27 +0800

    add colors and icons

--- packages/log_repository/lib/src/enums/log_action.dart
+++ packages/log_repository/lib/src/enums/log_action.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 /// Enum representing the type of action performed in a log entry.
 enum LogAction {
   /// Creation of a new resource.
@@ -18,6 +20,30 @@ enum LogAction {
         return 'Update';
       case LogAction.delete:
         return 'Delete';
+    }
+  }
+
+  /// Returns the color associated with this log action.
+  Color get color {
+    switch (this) {
+      case LogAction.create:
+        return Colors.green;
+      case LogAction.update:
+        return Colors.blue;
+      case LogAction.delete:
+        return Colors.red;
+    }
+  }
+
+  /// Returns the icon for this log action.
+  IconData get icon {
+    switch (this) {
+      case LogAction.create:
+        return Icons.add_rounded;
+      case LogAction.update:
+        return Icons.edit_rounded;
+      case LogAction.delete:
+        return Icons.delete_rounded;
     }
   }
 }
