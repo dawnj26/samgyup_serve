6cbc88baeab6bcf14f2d689079e60f133773357e
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2025-12-23 11:07:11 +0800

    add label text

--- lib/ui/components/name_input.dart
+++ lib/ui/components/name_input.dart
@@ -4,19 +4,21 @@ import 'package:samgyup_serve/ui/components/components.dart';
 class NameInput extends StatelessWidget {
   const NameInput({
     super.key,
+    this.labelText = 'Name',
     this.onChanged,
     this.initialValue,
     this.errorText,
   });
 
   final void Function(String name)? onChanged;
+  final String labelText;
   final String? initialValue;
   final String? errorText;
 
   @override
   Widget build(BuildContext context) {
     return OutlinedTextField(
-      labelText: 'Name',
+      labelText: labelText,
       onChanged: onChanged,
       initialValue: initialValue,
       errorText: errorText,
