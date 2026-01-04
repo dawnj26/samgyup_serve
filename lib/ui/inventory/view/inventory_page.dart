c56891de4104de177e49078fe58c5af0d12b90bd
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2026-01-04 16:48:15 +0800

    feat: add create category

--- lib/ui/inventory/view/inventory_page.dart
+++ lib/ui/inventory/view/inventory_page.dart
@@ -1,8 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:samgyup_serve/bloc/category/category_bloc.dart';
+import 'package:samgyup_serve/bloc/category/form/category_form_bloc.dart';
 import 'package:samgyup_serve/bloc/inventory/inventory_bloc.dart';
 import 'package:samgyup_serve/shared/dialog.dart';
+import 'package:samgyup_serve/shared/enums/loading_status.dart';
 import 'package:samgyup_serve/shared/snackbar.dart';
 import 'package:samgyup_serve/ui/inventory/view/inventory_screen.dart';
 
@@ -12,27 +15,70 @@ class InventoryPage extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<InventoryBloc, InventoryState>(
-      listener: (context, state) {
-        if (state is InventorySyncing) {
-          showLoadingDialog(context: context, message: 'Syncing inventory...');
-        }
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<InventoryBloc, InventoryState>(
+          listener: (context, state) {
+            if (state is InventorySyncing) {
+              showLoadingDialog(
+                context: context,
+                message: 'Syncing inventory...',
+              );
+            }
 
-        if (state is InventorySynced) {
-          context.router.pop();
-          showSnackBar(context, 'Synced successfully');
-        }
-      },
+            if (state is InventorySynced) {
+              context.router.pop();
+              showSnackBar(context, 'Synced successfully');
+            }
+          },
+        ),
+        BlocListener<CategoryFormBloc, CategoryFormState>(
+          listener: (context, state) {
+            final status = state.status;
+
+            if (status == LoadingStatus.loading) {
+              showLoadingDialog(context: context, message: 'Processing...');
+            }
+
+            if (status == LoadingStatus.success) {
+              context.router.pop();
+              showSnackBar(context, 'Cagtegory created successfully');
+              context.read<CategoryBloc>().add(const CategoryEvent.started());
+            }
+
+            if (status == LoadingStatus.failure) {
+              showErrorDialog(
+                context: context,
+                message: state.errorMessage ?? 'Something went wrong.',
+              );
+            }
+          },
+        ),
+      ],
       child: const InventoryScreen(),
     );
   }
 
   @override
   Widget wrappedRoute(BuildContext context) {
-    return BlocProvider(
-      create: (context) => InventoryBloc(
-        inventoryRepository: context.read(),
-      )..add(const InventoryEvent.started()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => InventoryBloc(
+            inventoryRepository: context.read(),
+          )..add(const InventoryEvent.started()),
+        ),
+        BlocProvider(
+          create: (context) => CategoryFormBloc(
+            inventoryRepository: context.read(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) =>
+              CategoryBloc(inventoryRepository: context.read())
+                ..add(const CategoryEvent.started()),
+        ),
+      ],
       child: this,
     );
   }
